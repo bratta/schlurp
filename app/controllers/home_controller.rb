73fbd4fb05aa3b6d6dc30757667f148b13f5331d
Tim Gourley <tim.gourley@blinqmedia.com>
2011-11-03 22:18:25 -0500

    Error catching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,28 @@ class HomeController < ApplicationController
     twitter_handle = params['t']
     @twitter_profile = Twitter.find_by_screen_name(twitter_handle)
     @twitter_profile = fetch_profile(twitter_handle) unless @twitter_profile
-    @twitpics = fetch_twitpics(twitter_handle)
-    @yfrogs = fetch_yfrogs(twitter_handle)
-    @social_images = [@twitpics, @yfrogs].flatten.shuffle
+    if @twitter_profile
+      @twitpics = fetch_twitpics(twitter_handle)
+      @yfrogs = fetch_yfrogs(twitter_handle)
+      @social_images = [@twitpics, @yfrogs].flatten.shuffle
+    end
   end
 
   private
 
   def fetch_profile(twitter_handle)
     url = "http://api.twitter.com/1/users/show.json?screen_name=#{twitter_handle}&include_entities=true"
-    response = RestClient.get(url)
+    begin
+      response = RestClient.get(url)
+    rescue
+      return nil
+    end
+    return nil unless response.code == 200
     parsed_response = JSON.parse(response.body)
     profile = Twitter.new
     profile.screen_name = twitter_handle
     %w( name profile_image_url profile_background_image_url url ).each do |field|
-      profile.send :hi, parsed_response[field]
+      profile.send "#{field}=", parsed_response[field]
     end
     profile.save
     profile
@@ -25,7 +32,12 @@ class HomeController < ApplicationController
 
   def fetch_twitpics(twitter_handle)
     url = "http://api.twitpic.com/2/users/show.json?username=#{twitter_handle}"
-    response = RestClient.get(url)
+    begin
+      response = RestClient.get(url)
+    rescue
+      return []
+    end
+    return [] unless response.code == 200
     parsed_response = JSON.parse(response.body)
     images = []
     parsed_response["images"].each do |image_obj|
@@ -36,7 +48,13 @@ class HomeController < ApplicationController
 
   def fetch_yfrogs(twitter_handle)
     url = "http://yfrog.com/api/userphotos.json?limit=25&screen_name=#{twitter_handle}&page=1&devkey=#{YFROG_API_KEY}"
-    response = RestClient.get(url)
+    response = nil
+    begin
+      response = RestClient.get(url)
+    rescue
+      return []
+    end
+    return [] unless response.code == 200
     parsed_response = JSON.parse(response.body)
     logger.info(parsed_response)
     images = []
