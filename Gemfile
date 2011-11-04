ba16e17d0117dd50f821057a29d0a5d88e01cf7a
Tim Gourley <tim.gourley@blinqmedia.com>
2011-11-03 20:06:39 -0500

    Initial Application Creation

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source :rubygems
+
+gem "sqlite3"
+gem "rails"
+gem "haml-rails"
+
+group :test, :development do
+  gem "factory_girl_rails"
+  gem "factory_girl_generator"
+  gem "rspec-rails"
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schlurp::Application.config.secret_token = 'bd1284586ffcad26b5e7bf70f15f3314ba493fd09622f059d426a80344282a5c544a9a84ba966d8d0ba7c1648751749afc0b3d0c6e6ffc3997e1df65be91d6b0'
